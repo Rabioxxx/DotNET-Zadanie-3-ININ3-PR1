60c43f37cbd10a14dfc7c2892566fa884c003f63
Rabioxxx <szg.jasinski@gmail.com>
2022-06-29 23:39:50 +0200

    Added Model.cs

---
+++ DotNET-Zadanie-3-ININ3-PR1/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNET_Zadanie_3_ININ3_PR1
+{
+    internal class Model
+    {
+    }
+}
