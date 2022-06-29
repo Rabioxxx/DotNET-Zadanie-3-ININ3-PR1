4a7bf4c107dce023a6f7dd9bab74977f0b047a9a
Rabioxxx <szg.jasinski@gmail.com>
2022-06-30 01:05:51 +0200

    Added second object, changed LinkedList to ObservableCollection

--- DotNET-Zadanie-3-ININ3-PR1/Model.cs
+++ DotNET-Zadanie-3-ININ3-PR1/Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,21 +9,28 @@ namespace DotNET_Zadanie_3_ININ3_PR1
 {
     internal class Model
     {
-        public LinkedList<MainListEntry> MainList { get; set; } = new LinkedList<MainListEntry>(new MainListEntry[]
+        public ObservableCollection<MainListEntry> MainList { get; set; } = new ObservableCollection< MainListEntry>(new MainListEntry[]
         {
             new MainListEntry(
                 "Transformers",
                 "Michael Bay",
                 "IDW Publishing",
                 "Blu ray disc",
-                DateTime.Parse("12.05.2007")
-                )
+                DateTime.Parse("2.5.2007")
+                ),
+            new MainListEntry(
+                "ABC",
+                "TEST",
+                "TESTing",
+                "dvd disc",
+                DateTime.Parse("12.10.2008")
+                ),
         });
 
         internal MainListEntry AddNewEntry()
         {
             MainListEntry entry = new MainListEntry();
-            MainList.AddLast(entry);
+            MainList.Add(entry);
             return entry;
         }
     }
